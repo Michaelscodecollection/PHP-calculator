faa2ad843fc4d7a05fbc5d6ae902a05b0394270b
Michael Nazareth <michaelnazareth@Katys-MacBook-Pro.local>
2022-07-10 12:43:58 -0600

    is this working?

---
+++ calculator.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Calculator</title>
+    </head>
+    <body>
+        <h1>PHP Calculator</h1>
+        <input type="number" name="num1">
+        <input type="number" name="num2">
+        <input type="submit">
+    </body>
+</html>
