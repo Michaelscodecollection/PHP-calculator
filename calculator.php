881c1232c6a0f59294027adf383bc58c2bbf743e
Michael Nazareth <michaelnazareth@Katys-MacBook-Pro.local>
2022-07-10 13:30:54 -0600

    php basic calculator

--- calculator.php
+++ calculator.php
@@ -5,9 +5,45 @@
         <title>PHP Calculator</title>
     </head>
     <body>
+        <form>
         <h1>PHP Calculator</h1>
-        <input type="number" name="num1">
-        <input type="number" name="num2">
-        <input type="submit">
+        <input type="number" name="num1" placeholder="Number 1">
+        <input type="number" name="num2" placeholder="Number 2">
+        <select name="operator">
+            <option>None</option>
+            <option>Add</option>
+            <option>Subtract</option>
+            <option>Divide</option>
+            <option>Multiply</option>
+        </select>
+        <br>
+        <button name="submit" value="submit" type="submit">Calculate</button>
+</form>
+<p> Product: 
+<?php
+    if (isset($_GET['submit'])) {
+            $result1 = $_GET['num1'];
+            $result2 = $_GET['num2'];
+            $operator = $_GET['operator'];
+
+            switch ($operator) {
+                case "None":
+                    echo "you must select a method";
+                    break;
+                case "Add":
+                    echo $result1 + $result2;
+                    break;
+                case "Subtract":
+                    echo $result1 - $result2;
+                    break;    
+                case "Divide":
+                    echo $result1 / $result2;
+                    break;
+                case "Multiply":
+                    echo $result1 * $result2;
+                    break;
+            }
+    } 
+?>
     </body>
 </html>
